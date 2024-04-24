b89ad101533de5f26b6132b5917ba5efaed8b94e
Manuel <manuelinfosec@gmail.com>
2024-04-24 15:18:28 +0100

    File rename & Finishing touches

---
+++ py/logocrawler/main.py
@@ -0,0 +1,83 @@
+"""A program that will crawl a list of website and output their logo URLs."""
+
+from typing import Optional
+
+import requests
+from scrape import download_image, parse_logo, save_image
+
+
+def collect_websites() -> list[str]:
+    """Collects a list of websites from STDIN."""
+    print("Enter a list of websites to scrape (separated by newline):")
+    websites = []
+    while True:
+        line = input()
+        if not line:
+            break
+        websites.append(line.strip())
+    return websites
+
+
+def prepend_http(website: str) -> str:
+    """Adds 'http://' to the website URL if it doesn't already start with it."""
+    if not website.startswith("http"):
+        website = "http://" + website
+    return website
+
+
+def fetch_page_source(website: str) -> Optional[str]:
+    """Fetches the page source code for a given website."""
+    try:
+        response = requests.get(website, timeout=5)
+        response.raise_for_status()  # raise an exception for non-200 status codes
+        return response.text
+    # handle possible network erros
+    except (
+        # used ChatGPT to determine each of these exceptions (without reading documentation)
+        requests.exceptions.RequestException,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.ConnectTimeout,
+    ) as e:
+        # TODO: Log website and error message for further probing
+        print(f"Error fetching page source for {website}: {e}")
+        return None
+
+
+def main():
+    """Program entry point."""
+    websites = collect_websites()
+
+    # TODO: CSV Headers
+    print("Starting scraping...\n")
+    for website in websites:
+        print(f"Website: {website}")
+
+        # add a schema to website
+        website = prepend_http(website)
+
+        # fetch page HTML source
+        page_source = fetch_page_source(website)
+
+        # if website is unreachable for scraping, use Public Logo API
+        if not page_source:
+            # download and save image to local drive
+            logo_bytes = download_image(website)
+            logo_url = save_image(logo_bytes, website)
+
+            # display path to local image
+            print("******************************************************************")
+            print(logo_url)
+
+            # skip to next website
+            continue
+
+        # parse page HTML and extract possible logo URL
+        logo_url = parse_logo(website, page_source)
+
+        # display full logo URL
+        print("******************************************************************")
+        print(logo_url)
+
+
+if __name__ == "__main__":
+    main()
--- py/logocrawler/scrape.py
+++ py/logocrawler/scrape.py
@@ -1,84 +1,165 @@
-"""A program that will crawl a list of website and output their logo URLs."""
+"""Utility module"""
 
-from typing import Optional
+import os
+import re
 
+import bs4
 import requests
-from utils import download_image, parse_logo, save_image
+
+# Constants
+CLEARBIT_URL = "https://logo.clearbit.com/"
+IMAGE_FORMATS = [".jpg", ".png", ".svg"]
 
 
-def collect_websites() -> list[str]:
-    """Collects a list of websites from STDIN."""
-    print("Enter a list of websites to scrape (separated by newline):")
-    websites = []
-    while True:
-        line = input()
-        if not line:
-            break
-        websites.append(line.strip())
-    return websites
+def append_schema(url: str, website: str):
+    """
+    Checks if a string contains a schema or append to it
+    """
+    print(url, website)
+    # allowed schema
+    allowed_schemas = ["http://", "https://"]
+
+    # check if the URL starts with an allowed schema
+    if not url.startswith(tuple(allowed_schemas)):
+        # write the website and schema to the beginning of the URI
+        url = website + url
+
+    return url
 
 
-def prepend_http(website: str) -> str:
-    """Adds 'http://' to the website URL if it doesn't already start with it."""
-    if not website.startswith("http"):
-        website = "http://" + website
-    return website
+def download_image(website: str) -> bytes:
+    """
+    Downloads image from a URL
+    """
 
+    logo_url = f"{CLEARBIT_URL}/{website}"
 
-def fetch_page_source(website: str) -> Optional[str]:
-    """Fetches the page source code for a given website."""
     try:
-        response = requests.get(website, timeout=5)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-        return response.text
-    # Handle possible network erros
+        # query ClearBit Public Logo APIs as a last resort
+        response = requests.get(logo_url, stream=True, timeout=5)
+        response.raise_for_status()
+        return response.content
     except (
         # Used ChatGPT to determine each of these exceptions (without reading documentation)
         requests.exceptions.RequestException,
         requests.exceptions.ConnectionError,
         requests.exceptions.ConnectTimeout,
     ):
-        # TODO: Log website and error message for further probing
-        # print(f"Error fetching page source for {website}: {e}")
         return None
 
 
-def main():
-    """Program entry point."""
-    websites = collect_websites()
+def strip_illegal(url: str):
+    """Strips a URL for safe use as a file name"""
+    # characters not allowed in a filename
+    illegal_chars = ["<", ">", ":", '"', "/", "\\", "|", "?", "*"]
 
-    # TODO: CSV Headers
-    print("Starting scraping...\n")
-    for website in websites:
-        print(f"Website: {website}")
+    # replace illegal characters with underscores
+    for char in illegal_chars:
+        url = url.replace(char, "_")
 
-        # add a schema to website
-        website = prepend_http(website)
+    # remove trailing dots and schema
+    url = url.strip(". ").replace("http___", "").replace("https___", "")
 
-        # fetch page HTML source
-        page_source = fetch_page_source(website)
-
-        # If website is unreachable for scraping, use Public Logo API
-        if page_source is None:
-
-            # download and save image to local drive
-            logo_bytes = download_image(website)
-            logo_url = save_image(logo_bytes)
-
-            # display path to local image
-            print("******************************************************************")
-            print(logo_url)
-
-            # skip to next website
-            continue
-
-        # parse page HTML and extract possible logo URL
-        logo_url = parse_logo(website, page_source)
-
-        # Display full logo URL
-        print("******************************************************************")
-        print(logo_url)
+    return url
 
 
-if __name__ == "__main__":
-    main()
+def save_image(
+    image_data: bytes, file_name: str, data_folder: str = "data"
+) -> str | None:
+    """
+    Saves the downloaded image data to `data` folder
+    """
+    # create `data` folder
+    os.makedirs(data_folder, exist_ok=True)
+
+    # construct file path and strip illegal characters
+    filename = f"image_{strip_illegal(file_name)}.jpg"
+    image_path = os.path.join(data_folder, filename)
+
+    # save image to the file
+    try:
+        with open(image_path, "wb") as f:
+            f.write(image_data)
+
+        return image_path
+    except OSError as e:
+        print(f"Error saving image: {e}")
+        return None
+
+
+def is_image_tag(tag: bs4.element.Tag) -> bool:
+    """Checks if a tag is of image-related type"""
+    return tag.name == "img" or tag.name == "svg" or tag.name == "picture"
+
+
+def parse_logo(website: str, source: str) -> str:
+    """
+    Parses HTML page source (source) and extracts the logo URL.
+
+    Function uses an "early return" pattern to avoid executing code further
+    in the case of a pattern match
+    """
+    soup = bs4.BeautifulSoup(source, features="html.parser")
+
+    logo_url: str | None = None
+
+    # 1. Check for the first class that has the string "logo" in it.
+    try:
+        logo_class: bs4.element.Tag = soup.find(class_=re.compile(".*logo.*"))
+        if logo_class and is_image_tag(logo_class):
+            print("Logo Class detected")
+            logo_url = logo_class.get("src")
+    except (AttributeError, TypeError) as e:
+        print("Error extracting logo class: ", e)
+
+    # 2. Check the first value that has the string "logo" in src.
+    if not logo_url:
+        try:
+            logo_src: bs4.element.Tag = soup.find(src=re.compile(".*logo.*"))
+            if logo_src and is_image_tag(logo_src):
+                print("Logo Src Detected")
+                logo_url = logo_src.get("src")
+        except (AttributeError, TypeError) as e:
+            print("Error extracting logo `src`: ", e)
+
+    # 3. Collect the meta property of "og:image"
+    if not logo_url:
+        try:
+            og_image: bs4.element.Tag = soup.find("meta", property="og:image")
+            if og_image:
+                print("OG Meta detected")
+                logo_url = og_image.get("content")
+        except (AttributeError, TypeError) as e:
+            print("Error extracting og:image meta: ", e)
+
+    # 4. Check the first alt attribute with the string "logo" in it.
+    if not logo_url:
+        try:
+            logo_alt: bs4.element.Tag = soup.find(attrs={"alt": re.compile(".*logo.*")})
+            if logo_alt and is_image_tag(logo_alt):
+                print("Logo Alt Detected")
+                logo_url = logo_alt.get("src")
+        except (AttributeError, TypeError) as e:
+            print("Error extracting logo alt: ", e)
+
+    # UNUSED: 5. Collect the first .jpg, .png, .svg on the site.
+    # if not logo_url:
+    #     try:
+    #         # Find the first <img> tag filter from IMAGE_FORMATS a match for...
+    #         first_image_tag: bs4.element.Tag = soup.find(
+    #             src=lambda s: any(format in s for format in IMAGE_FORMATS)
+    #         )
+    #         logo_url = first_image_tag["src"]
+    #     except (AttributeError, TypeError) as e:
+    #         print("Error extracting first image: ", e)
+
+    # 6. Final Resort: Query Public Logo API
+    if not logo_url:
+        # download and save image to local drive
+        logo_bytes = download_image(website)
+        logo_url = save_image(logo_bytes, website) if logo_bytes else None
+
+    # UNUSED: Check if `logo_url` starts with schema then append the website name.
+    # logo_url = append_schema(logo_url, website)
+
+    return logo_url
